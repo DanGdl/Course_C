726e8933b10c24f45d315f019a06f18c8f4c2ca6
DanGld <dangladste@gmail.com>
2024-07-22 19:39:32 +0300

    daemon example

---
+++ ex20_daemon/daemon.h
@@ -0,0 +1,13 @@
+#ifndef H_DGD_LOGGER
+#define H_DGD_LOGGER
+
+#define FILE_NOT_FOUND -2
+#define MODE_DAEMON_OFF "-d"
+
+#define PATH_CONFIG "/etc/ots_daemon.conf"
+
+#define TAG_CONFIG_LOCK "lockfile"
+#define TAG_CONFIG_SOCKET "socket"
+#define TAG_CONFIG_FILE "checkfile"
+
+#endif
